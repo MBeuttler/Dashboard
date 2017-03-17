18e309b90577f567603c465580ac7e216cf7e268
Roy Knaebel <roy.knaebel@de.trumpf.com>
2017-03-17 14:25:54 +0100

    delete dashboard backend api

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ var path = require('path');
 
 var app = express();
 
-var drinks = require('./routes/drinks');
-var juices = require('./routes/juices');
-var workload = require('./routes/workload');
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/app', express.static(__dirname + '/public/app'));
 app.use('/css', express.static(__dirname + '/public/css'));
-
-app.use('/api/drinks', drinks);
-app.use('/api/juices', juices);
-//app.use('/api/workloads', workload)
 
 app.all('/api/*', function(req, res, next) {
   res.sendStatus(404);
